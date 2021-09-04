fa0c0036b83e3395a0abb6cd95d6416a5f7fba78
JAlbert2 <jonah.albert2@gmail.com>
2021-09-04 09:24:45 -0400

    Final push

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -24,14 +24,52 @@ export class HelloWorld extends LitElement {
     this.counter = 5;
   }
 
+  colorChange() {
+    let color = 0;
+    if (this.counter < 26) {
+      color = `color: rgb(${this.counter * 10},0,0)`;
+    } else if (this.counter > 25 && this.counter < 51) {
+      color = `color: rgb(${250 - (this.counter - 25) * 10},${
+        (this.counter - 25) * 10
+      },0)`;
+    } else if (this.counter > 50) {
+      color = `color: rgb(0,${250 - (this.counter - 50) * 10},${
+        (this.counter - 50) * 10
+      })`;
+    }
+    this.shadowRoot.getElementById('h2').style = color;
+  }
+
   __increment() {
     this.counter += 1;
+    console.log('Up');
+    if (this.counter > 0) {
+      this.shadowRoot.getElementById('down').disabled = false;
+    }
+    this.colorChange();
+  }
+
+  __decrement() {
+    this.counter -= 1;
+    console.log('Down');
+    if (this.counter < 1) {
+      this.shadowRoot.getElementById('down').disabled = true;
+    }
+    this.colorChange();
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <h2 id="h2">${this.title} Nr. ${this.counter}!</h2>
+      <button id="up" @click=${this.__increment}>increment</button>
+      <button id="down" @click=${this.__decrement}>decrement</button>
+      <br />
+      <input
+        id="input"
+        type="number"
+        .value="${this.counter}"
+        disabled="true"
+      />
     `;
   }
 }
